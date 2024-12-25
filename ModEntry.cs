b10b16519069d11353f2520f6fec77077173698b
Vladyslav Harkusha <harkushavlad823@gmail.com>
2024-12-25 12:32:34 +0200

    first commit

---
+++ ModEntry.cs
@@ -0,0 +1,11 @@
+using StardewModdingAPI;
+
+namespace MultiplayerCalendarPlanner
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
